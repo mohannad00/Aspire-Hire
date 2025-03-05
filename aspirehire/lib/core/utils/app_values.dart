c791b7909e4085d8e2146cafda6a519b3feda30a
shahd <shahdy018@gmail.com>
2025-03-06 01:09:34 +0200

    edited reusable components

---
+++ aspirehire/lib/core/utils/app_values.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppValues {
+  static double screenHeight = ScreenUtil().screenHeight;
+  static double screenWidth = ScreenUtil().screenWidth;
+
+  static double marginHeight = 1.h; 
+  static double paddingHeight = 1.h;
+  static double sizeHeight = 1.h;
+  static double marginWidth = 1.w;
+  static double paddingWidth = 1.w;
+  static double sizeWidth = 1.w;
+  static double font = 1.sp;
+  static double radius = 1.r;
+}
